43e44941dc34a0b66ebdece02e31d6e8cff82cd8
Thriven Kumar Yanamandram <thriven.yanamandram@daikinapplied.com>
2025-02-15 23:59:50 -0600

    Removed obj folder

--- PlatformServices/obj/Debug/net9.0/PlatformServices.AssemblyInfo.cs
+++ PlatformServices/obj/Debug/net9.0/PlatformServices.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformServices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3aa9908391d4afb4bb89219cd6257bc386720a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+237ce15c7ed99cd01ad8ecef3311224cee0f613b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformServices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformServices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
